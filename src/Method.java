5b6ea862f3dcea80a954c29a0b1af54a004a8f94
User <User@Live_126534>
2021-07-15 18:57:56 +0900

    Secondary Commit

--- src/Method.java
+++ src/Method.java
@@ -2,5 +2,19 @@
 public class Method {
 	public static void main(String[]args){
 		MethodExam mc = new MethodExam();
+		// MethodExam이라는 참조형 data type. mc라는 참조형 변수명. new는 다음 것이 생성자, 곧 MethodExam이라는 instance 생기게 함
+		mc.method1();
+		// period는 mc이라는 변수가 가진 클래스(속성, 메소드)를 접근가능
+		// 전에 만든 method1을 실행하게 한다. 괄호 사이에 아무것도 없다는 것은 매개변수가 없기 때문
+		mc.method2(10);
+		// 전에 만든 method2를 실행하게 한다. 괄호 사이의 10은 정수형 (integer) 매개변수이다
+		int value = mc.method3();
+		// 전에 만든 method3는 매개변수를 만들지 않아 괄호 사이에 값이 없음. 하지만 정수형 리턴값이 있지만 받아낼 방법이 없다
+		// 그렇기에 값을 받아낼 변수 생성이 필요하다. 이게 바로 int value이다.
+		System.out.println("method3가 리턴한 값" + value);
+		mc.method4(3, 5);
+		// 전에 만든 method4는 정수형 매개변수를 두개 받는다. 그렇기에 괄호 사이에 정수형 피연산자 두개가 있다
+		int value2 = mc.method5(5);
+		System.out.println("method5가 리턴한 값" + value2);
 	}
 }
--- src/MethodExam.java
+++ src/MethodExam.java
@@ -9,7 +9,7 @@ public class MethodExam {
 	// void는 리턴타입이 없다는 것을 의미함. method1는 윗 메소드의 이름이다. 괄호() 사이에 아무것도 없다는 것은 매개변수가 없다를 의미함
 	//2. 들어오는 매개변수는 있지만, 무슨 task를 수행한 다음 리턴값이 없다
 	public void method2 (int i) {
-		System.out.println("m2이 실행되었습니다.");
+		System.out.println(i + "를 사용한 m2이 실행되었습니다.");
 	}
 	// 괄호() 사이에 int i가 있다는 것은 i라는 integer형태의 매개변수를 받았다는 뜻이다
 	// 3. 들어오는 매개변수는 없지만, 무슨일을 수행한 다음에 리턴값을 돌려준다
@@ -21,12 +21,12 @@ public class MethodExam {
 	// return이라는 keyword는 값을 되돌려주겠다는 예약어
 	// 4. 매개면수 2개를 (아니면 그 이상을) 받고 값을 리턴 안한다
 	public void method4 (int i, int x) {
-		System.out.println("m4이 실행되었습니다.");
+		System.out.println(i + "과" + x + "를 사용한 m4이 실행되었습니다.");
 	}
 	// 매개변수는 얼마든지 괄호 사이에 나열 가능하다. (Comma로 연결가능). 참조형 data type들도 잆력 가능하다
 	// 5. 값을 바다아서 값을 출력하는 형태
 	public int method5 (int i) {
-		System.out.println("m5이 실행되었습니다.");
+		System.out.println(i + "를 사용한 m5이 실행되었습니다.");
 		return i*2;
 	}
 }
